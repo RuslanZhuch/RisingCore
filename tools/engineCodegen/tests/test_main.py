38f7d7c2528ab9aec0d07694e5bfdd867b5cc379
RuslanZhuch <ruslanacs93@mail.ru>
2023-07-21 23:53:04 +0200

    Tests fix

--- tools/engineCodegen/tests/test_main.py
+++ tools/engineCodegen/tests/test_main.py
@@ -1,11 +1,15 @@
 import unittest
-from pathlib import Path
+import shutil
+
+import os
 
 def _remove_dest_files():
-    [f.unlink() for f in Path("dest").glob("*") if f.is_file()] 
+    if os.path.exists('dest'):
+        shutil.rmtree('dest')
 
 if __name__ == '__main__':
     _remove_dest_files()
     
     testsuite = unittest.TestLoader().discover('.')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
+    
--- tools/engineCodegen/tests/test_project.py
+++ tools/engineCodegen/tests/test_project.py
@@ -56,6 +56,8 @@ class TestProject(unittest.TestCase):
         self.assertEqual(project_desc.executors_paths[2], "executor3.json")
         
     def test_gen_project1(self):
+        if os.path.exists('dest/projects/project1'):
+            shutil.rmtree('dest/projects/project1')
         [f.unlink() for f in Path("assets/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/workspace/prev").glob("*") if f.is_file()] 
@@ -87,6 +89,14 @@ class TestProject(unittest.TestCase):
         utils.assert_files(self, "dest/projects/project1/runtime.cpp", "assets/expected/runtime.cpp")
                 
     def test_gen_project2_with_diff(self):
+        if os.path.exists('dest/projects/project2'):
+            shutil.rmtree('dest/projects/project2')
+        
+        [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
+        project.generate("assets/project2/project2.json")
+        
         [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
@@ -96,25 +106,12 @@ class TestProject(unittest.TestCase):
         
         contexts_path = "assets/project2/contexts"
         prev_contexts_path = contexts_path + "/prev"
-        if not os.path.exists(contexts_path):
-            os.makedirs(contexts_path)
-        if not os.path.exists(prev_contexts_path):
-            os.makedirs(prev_contexts_path)
             
         executors_path = "assets/project2/executors"
         prev_executors_path = executors_path + "/prev"
-        if not os.path.exists(executors_path):
-            os.makedirs(executors_path)
-        if not os.path.exists(prev_executors_path):
-            os.makedirs(prev_executors_path)
-        
+                    
         shutil.copyfile("assets/expected/project2/prev/lcontext2.json", prev_contexts_path + "/lcontext2.json")
         shutil.copyfile("assets/expected/project2/prev/executor1.json", prev_executors_path + "/executor1.json")
-        
-        shutil.copyfile("assets/expected/project2/contexts/lContext2Context.h", "dest/projects/project2/contexts/LContext2Context.h")
-        shutil.copyfile("assets/expected/project2/contexts/lContext2Context.cpp", "dest/projects/project2/contexts/LContext2Context.cpp")
-        shutil.copyfile("assets/expected/project2/executors/Executor1Executor.h", "dest/projects/project2/executors/Executor1Executor.h")
-        shutil.copyfile("assets/expected/project2/executors/Executor1Executor.cpp", "dest/projects/project2/executors/Executor1Executor.cpp")
         
         prev_context_mod_time = get_file_modification_time(prev_contexts_path + "/lcontext2.json")
         prev_executor_mod_time = get_file_modification_time(prev_executors_path + "/executor1.json")
@@ -168,6 +165,14 @@ class TestProject(unittest.TestCase):
         self.assertNotEqual(get_file_modification_time(prev_root_path + "/ws_applicationContext.json"), max_initial_modification_time)
    
     def test_gen_project2_with_diff_2(self):
+        if os.path.exists('dest/projects/project2'):
+            shutil.rmtree('dest/projects/project2')
+        
+        [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
+        project.generate("assets/project2/project2.json")
+        
         [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
@@ -177,17 +182,9 @@ class TestProject(unittest.TestCase):
         
         contexts_path = "assets/project2/contexts"
         prev_contexts_path = contexts_path + "/prev"
-        if not os.path.exists(contexts_path):
-            os.makedirs(contexts_path)
-        if not os.path.exists(prev_contexts_path):
-            os.makedirs(prev_contexts_path)
             
         executors_path = "assets/project2/executors"
         prev_executors_path = executors_path + "/prev"
-        if not os.path.exists(executors_path):
-            os.makedirs(executors_path)
-        if not os.path.exists(prev_executors_path):
-            os.makedirs(prev_executors_path)
         
         shutil.copyfile("assets/expected/project2/prev/lcontext1.json", prev_contexts_path + "/lcontext1.json")
         shutil.copyfile("assets/expected/project2/prev/lContext2_modified.json", prev_contexts_path + "/lContext2.json")
@@ -255,6 +252,14 @@ class TestProject(unittest.TestCase):
         self.assertNotEqual(get_file_modification_time(prev_root_path + "/ws_applicationContext.json"), pre_generation_time)
 
     def test_gen_project2_with_diff_workspace_structure(self):
+        if os.path.exists('dest/projects/project2'):
+            shutil.rmtree('dest/projects/project2')
+        
+        [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
+        project.generate("assets/project2/project2.json")
+        
         [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
@@ -265,6 +270,9 @@ class TestProject(unittest.TestCase):
             os.makedirs(root_path)
         if not os.path.exists(prev_root_path):
             os.makedirs(prev_root_path)
+            
+        if not os.path.exists("dest/projects/project2"):
+            os.makedirs("dest/projects/project2")
             
         shutil.copyfile("assets/expected/project2/prev/ws_applicationContext.json", prev_root_path + "/ws_applicationContext.json")
         shutil.copyfile("assets/expected/project2/prev/project2.json", prev_root_path + "/project2.json")
@@ -305,6 +313,14 @@ class TestProject(unittest.TestCase):
         self.assertGreater(get_file_modification_time("dest/projects/project2/contexts/LContext1Context.cpp"), prev_runtime_mod_time)
         
     def test_gen_project2_with_no_prev_workspace_structure(self):
+        if os.path.exists('dest/projects/project2'):
+            shutil.rmtree('dest/projects/project2')
+        
+        [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
+        project.generate("assets/project2/project2.json")
+        
         [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
@@ -348,6 +364,13 @@ class TestProject(unittest.TestCase):
         self.assertGreater(get_file_modification_time("dest/projects/project2/contexts/LContext1Context.cpp"), pre_generation_time)
 
     def test_gen_project2_with_diff_project_structure(self):
+        shutil.rmtree('dest/projects/project2')
+        
+        [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
+        [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
+        project.generate("assets/project2/project2.json")
+        
         [f.unlink() for f in Path("assets/project2/contexts/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/executors/prev").glob("*") if f.is_file()] 
         [f.unlink() for f in Path("assets/project2/prev").glob("*") if f.is_file()] 
